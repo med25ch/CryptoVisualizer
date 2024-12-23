aba81925cf08ad291f86b7f2d111df525e1c82f4
med25ch <medresse25@gmail.com>
2024-12-22 19:54:06 -0500

    Commit : Handling Network Errors and showing them in the snackbar.

--- app/src/main/java/com/resse/cryptovisualizer/MainActivity.kt
+++ app/src/main/java/com/resse/cryptovisualizer/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.resse.cryptovisualizer
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.resse.cryptovisualizer.core.presentation.util.ObserveAsEvents
+import com.resse.cryptovisualizer.core.presentation.util.toString
+import com.resse.cryptovisualizer.crypto.presentation.coin_list.CoinListEvent
 import com.resse.cryptovisualizer.crypto.presentation.coin_list.CoinListScreen
 import com.resse.cryptovisualizer.crypto.presentation.coin_list.CoinListViewModel
 import com.resse.cryptovisualizer.ui.theme.CryptoTrackerTheme
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 import org.koin.androidx.compose.viewModel
 import org.koin.androidx.viewmodel.factory.KoinViewModelFactory
@@ -26,10 +36,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CryptoTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                val scope = rememberCoroutineScope()
+                val snackbarHostState = remember { SnackbarHostState() }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+                ) { innerPadding ->
 
                     val viewModel = koinViewModel<CoinListViewModel>()
                     val state by viewModel.state.collectAsStateWithLifecycle()
+                    val context = LocalContext.current
+
+
+                    ObserveAsEvents(events = viewModel.events) { event ->
+                        when(event) {
+                            is CoinListEvent.Error -> {
+                                scope.launch {
+                                    snackbarHostState.showSnackbar(event.error.toString(context))
+                                }
+                            }
+                        }
+                    }
 
                     CoinListScreen(
                         state = state,
