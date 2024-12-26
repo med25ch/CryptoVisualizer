53da6547144bdbd3cb7455df39c23545b59a7007
med25ch <medresse25@gmail.com>
2024-12-26 13:27:42 -0500

    Commit : Loading Coin Details

--- app/src/main/java/com/resse/cryptovisualizer/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/resse/cryptovisualizer/crypto/data/networking/RemoteCoinDataSource.kt
@@ -6,11 +6,17 @@ import com.resse.cryptovisualizer.core.domain.util.NetworkError
 import com.resse.cryptovisualizer.core.domain.util.Result
 import com.resse.cryptovisualizer.core.domain.util.map
 import com.resse.cryptovisualizer.crypto.data.mappers.toCoin
+import com.resse.cryptovisualizer.crypto.data.mappers.toCoinPrice
+import com.resse.cryptovisualizer.crypto.data.networking.dto.CoinHistoryDto
 import com.resse.cryptovisualizer.crypto.data.networking.dto.CoinsResponseDto
 import com.resse.cryptovisualizer.crypto.domain.Coin
 import com.resse.cryptovisualizer.crypto.domain.CoinDataSource
+import com.resse.cryptovisualizer.crypto.domain.CoinPrice
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RemoteCoinDataSource (
     private val httpClient: HttpClient
@@ -22,6 +28,34 @@ class RemoteCoinDataSource (
             )
         }.map { response ->
             response.data.map { it.toCoin() }
+        }
+    }
+
+    override suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError> {
+
+        val startMillis = start
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+        val endMillis = end
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+
+        return safeCall<CoinHistoryDto> {
+            httpClient.get(
+                urlString = constructUrl("/assets/$coinId/history")
+            ) {
+                parameter("interval", "h6")
+                parameter("start", startMillis)
+                parameter("end", endMillis)
+            }
+        }.map { response ->
+            response.data.map { it.toCoinPrice() }
         }
     }
 }
--- app/src/main/java/com/resse/cryptovisualizer/crypto/domain/CoinDataSource.kt
+++ app/src/main/java/com/resse/cryptovisualizer/crypto/domain/CoinDataSource.kt
@@ -2,7 +2,14 @@ package com.resse.cryptovisualizer.crypto.domain
 
 import com.resse.cryptovisualizer.core.domain.util.NetworkError
 import com.resse.cryptovisualizer.core.domain.util.Result
+import java.time.ZonedDateTime
 
 interface CoinDataSource {
     suspend fun getCoins(): Result<List<Coin>, NetworkError>
+
+    suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError>
 }
